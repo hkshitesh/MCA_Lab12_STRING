bfe055db0971592f84d7c41165d0477fb0016bac
Hitesh <hkshitesh@gmail.com>
2022-03-12 09:27:09 +0530

    first commit

---
+++ src/pkg1/Lab12_Ex1.java
@@ -0,0 +1,19 @@
+package pkg1;
+
+public class Lab12_Ex1 {
+
+	public static void main(String[] args) {
+
+ String s1 = "Hello upes";
+ char c[]= {'h','e','l','l','o'};
+ String s2= new String("Hello"); 
+ System.out.println(s1);
+ System.out.println(c);
+ System.out.println(s2);
+ int len=s1.length();
+ System.out.println("length of s1 is "+len); 
+ System.out.println(s1.substring(2,6));
+ System.out.println(s1.charAt(7));
+ System.out.println(s1.toUpperCase()); 
+	}
+}
