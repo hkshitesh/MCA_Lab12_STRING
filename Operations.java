580b56fbf9ca1dc148c17d2c7f51f71cf3df06d1
mansi <abc@example.com>
2022-10-30 20:53:12 +0530

    files added

---
+++ Operations.java
@@ -0,0 +1,16 @@
+package MyCustomPackage;
+class Operations{
+    public void sum(int a, int b){
+        System.out.printf("Sum: %d" ,a+b);
+    }
+    public void multiply(int a, int b){
+        System.out.println("Product: "+ a*b);
+    }
+    public void division(int a, int b){
+        System.out.println("Division: "+ a/b);
+    }
+    public void difference(int a, int b){
+        System.out.println("Difference: "+ (a-b));
+    }
+}
+
